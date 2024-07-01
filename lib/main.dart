af5466ee9588f1ffc37df9369bde42989ef7a0c9
rambutan <pride.kraievskyi@gmail.com>
2024-07-01 11:37:03 +0300

    install Google fonts && CategoriesScreen basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:meals_app/categories.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Meals Flutter',
-    theme: ThemeData(
-      useMaterial3: true,
-    ),
-    home: const Column(
-      children: [
-        Text('Hi here')
-      ],
-    ),
-  ));
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals Flutter',
+      theme: theme,
+      home: const CategoriesScreen(),
+    );
+  }
 }
