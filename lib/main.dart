3810bd8be1ee973feae459fa9bc62c0374212d14
rambutan <pride.kraievskyi@gmail.com>
2024-07-01 12:56:58 +0300

    add Route and Navigation to Meals from CategoryScreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:meals_app/screens/categories.dart';
-import 'package:meals_app/screens/meals.dart';
-import 'package:meals_app/data/dummy_data.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -26,7 +24,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Meals Flutter',
       theme: theme,
-      home: const MealsScreen(title: 'Some category...', meals: dummyMeals),
+      home: const CategoriesScreen(),
     );
   }
 }
