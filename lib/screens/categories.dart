fcf43fa095ddba2dd41de210a74831e2da486ae8
rambutan <pride.kraievskyi@gmail.com>
2024-07-01 12:16:02 +0300

    add layout category_grid_item

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
@@ -16,31 +19,9 @@ class CategoriesScreen extends StatelessWidget {
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text(
-            '1',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '2',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '3',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '4',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '5',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '6',
-            style: TextStyle(color: Colors.white),
-          ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
