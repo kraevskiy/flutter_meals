ead0f0eb2b1002416a1c9589517316eb4d92af8e
rambutan <pride.kraievskyi@gmail.com>
2024-07-03 13:48:01 +0300

    add filters store PopScope && pop(Filters)

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -51,14 +51,15 @@ class _TabsScreen extends State<TabsScreen> {
     });
   }
 
-  void _setScreen(String identifier) {
+  void _setScreen(String identifier) async {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
-      Navigator.of(context).push(
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
         MaterialPageRoute(
           builder: (ctx) => const FiltersScreen(),
         ),
       );
+
     }
   }
 
