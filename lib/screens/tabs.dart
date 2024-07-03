63f69dcb6fecda00f39f2ef722f65ef6a6898633
rambutan <pride.kraievskyi@gmail.com>
2024-07-03 13:26:09 +0300

    add connect filter screen

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 
 import 'package:meals_app/models/meal.dart';
@@ -51,10 +52,13 @@ class _TabsScreen extends State<TabsScreen> {
   }
 
   void _setScreen(String identifier) {
+    Navigator.of(context).pop();
     if (identifier == 'filters') {
-
-    } else {
-      Navigator.of(context).pop();
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const FiltersScreen(),
+        ),
+      );
     }
   }
 
