127801eded272d290bd579ce005012bff3175f4c
rambutan <pride.kraievskyi@gmail.com>
2024-07-03 13:26:17 +0300

    add connect filter screen

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:meals_app/screens/tabs.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
+
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
 
@@ -18,6 +21,18 @@ class _FiltersScreen extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
+      // drawer: MainDrawer(
+      //   onSelectedScreen: (identifier) {
+      //     Navigator.of(context).pop();
+      //     if (identifier == 'meals') {
+      //       Navigator.of(context).pushReplacement(
+      //         MaterialPageRoute(
+      //           builder: (ctx) => const TabsScreen(),
+      //         ),
+      //       );
+      //     }
+      //   },
+      // ),
       body: Column(
         children: [
           SwitchListTile(
