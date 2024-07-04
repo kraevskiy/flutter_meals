ad7919c9d2c0e1c0eff49e58cf7aeb5427815b21
rambutan <pride.kraievskyi@gmail.com>
2024-07-04 14:00:05 +0300

    add favoritesMeals provider

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:meals_app/models/meal.dart';
+
+class FavoritesMealsNotifier extends StateNotifier<List<MealModel>> {
+  FavoritesMealsNotifier() : super([]);
+
+  void toggleMealFavoriteStatus(MealModel meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoritesMealsProvider = StateNotifierProvider((ref) {
+  return FavoritesMealsNotifier();
+});
